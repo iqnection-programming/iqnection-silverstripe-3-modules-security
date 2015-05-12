006a8d43793a0bbdff6ed8365a77fef560934860
meckert <meckert@iqnection.com>
2015-05-12 14:58:32 -0400

    uncomment from testing

--- code/IQLeftAndMainDecorator.php
+++ code/IQLeftAndMainDecorator.php
@@ -8,7 +8,7 @@ class IQLeftAndMainDecorator extends LeftAndMainExtension {
 	
 	function init() 
 	{
-		//if (!Permission::check('ADMIN'))
+		if (!Permission::check('ADMIN'))
 		{
 			Requirements::javascript('iq-security/javascript/iq-security.js');
 			Requirements::css('iq-security/css/iq-security.css');
