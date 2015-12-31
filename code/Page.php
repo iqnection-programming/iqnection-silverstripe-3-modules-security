9d0d9a4b4648b055646015fb72e4352aee218c7f
helpfulrobot <chris@silverstripe.com>
2015-12-31 23:48:07 +1300

    Converted to PSR-2

--- code/Page.php
+++ code/Page.php
@@ -1,12 +1,14 @@
 <?php
 
 
-	class IQSecurity_Page extends Extension
-	{
-		function canCreate($Member=null)
-		{			
-			$SiteConfig = DataObject::get_one('SiteConfig');
-			if (in_array($this->owner->ClassName,explode(',',$SiteConfig->BlockPageTypes))) return false;
-			return true;
-		}
-	}
+    class IQSecurity_Page extends Extension
+    {
+        public function canCreate($Member=null)
+        {
+            $SiteConfig = DataObject::get_one('SiteConfig');
+            if (in_array($this->owner->ClassName, explode(',', $SiteConfig->BlockPageTypes))) {
+                return false;
+            }
+            return true;
+        }
+    }
